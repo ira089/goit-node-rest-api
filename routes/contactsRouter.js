798e62958debc955acfa86de868967c537aa38fc
ira089 <gvir2015@gmail.com>
2024-03-15 21:11:14 +0200

    nodaHW02_fin1

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -6,6 +6,9 @@ import {
   createContact,
   updateContact,
 } from "../controllers/contactsControllers.js";
+import validateBody from '../helpers/validateBody.js'
+import * as contactsSchemas from '../schemas/contactsSchemas.js'
+
 
 const contactsRouter = express.Router();
 
@@ -15,8 +18,8 @@ contactsRouter.get("/:id", getOneContact);
 
 contactsRouter.delete("/:id", deleteContact);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post("/", validateBody(contactsSchemas.createContactSchema), createContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put("/:id", validateBody(contactsSchemas.updateContactSchema), updateContact);
 
 export default contactsRouter;
