4bd1dcb32eb91db2f58c9bb5b6f8908a5855c793
ira089 <gvir2015@gmail.com>
2024-03-22 23:22:12 +0200

    hw03_env

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 import contactsRouter from "./routes/contactsRouter.js";
+
+dotenv.config();
+const {DB_HOST, PORT = 3000} = process.env;
 
 const app = express();
 
@@ -21,6 +26,20 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running. Use our API on port: 3000");
+// });
+
+// const DB_HOST = 'mongodb+srv://gvir2015:PEurQbs6Y9DEyDC4@cluster0.8tqm2h7.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(DB_HOST)
+.then(()=> {
+  app.listen(PORT, () => {
+    console.log("Database connection successful");
+  });
+  
+})
+.catch(error => {
+  console.error('Server is running. Use our API on port')
+  // console.error(`Server is running. Use our API on port: ${PORT}`)
+  process.exit(1)
+})
