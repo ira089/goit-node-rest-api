cd3dbd573099810322a37c738e6a4657c75d01ce
ira089 <gvir2015@gmail.com>
2025-03-24 20:32:23 +0200

    seedAdmin

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,13 @@
-import seedSectors from "./seedSectors";
+// import seedSectors from "./seedSectors";
 // import seedSkills from "./seedSkills";
 // import seedPositions from "./seedPositions";
+import seedAdmin from "./seedAdmin";
 
 async function main() {
-  await seedSectors();
+  // await seedSectors();
   // await seedSkills();
   // await seedPositions();
+  await seedAdmin();
 }
 
 main().catch(console.error);
--- prisma/seedAdmin.ts
+++ prisma/seedAdmin.ts
@@ -7,14 +7,28 @@ async function seedAdmin() {
     create: {
       email: "darinataranenko@gmail.com",
       target: "MENTOR",
-      firstName: "",
-      lastName: "",
-      phone: "",
+      firstName: "Даріна",
+      lastName: "Тараненко",
+      phone: "+380960989058",
       isAdmin: true,
-      scheduleStart: "",
-      scheduleEnd: "",
+      scheduleStart: "09.00",
+      scheduleEnd: "18.00",
+      password: "Qwer*1234",
+      linkedin: "https://www.linkedin.com/in/darina-taranenko-48b899146/",
+      slack: "https://unkdworkspace.slack.com/team/U071E5XPX55",
+      telegram: "https://t.me/tarodera",
     },
   });
+  console.log("Admin seeded successfully!");
 }
+
+seedAdmin()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
 
 export default seedAdmin;
